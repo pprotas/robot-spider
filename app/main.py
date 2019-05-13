0756a82f5aedc624e7a20dcfca58cb6638027141
Pawel Protas <pawprotas@gmail.com>
2019-05-13 09:44:25 +0200

    Merge conflict fix

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from movement.movement import Movement
 import time
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     movement = Movement(4)
     movement.move_servo(111,111)
 ##    while True:
@@ -13,8 +12,3 @@ if __name__ == '__main__':
 ##        Position = movement.readNumber()
 ##        print("Position: ", Position*1023/255)
 ##        time.sleep(2)
-    
-=======
-    movement = Movement(0x4)
-    movement.move_servo(1, 0)
->>>>>>> 747e1c33e84229024897a84830139cd8108bb0bc
--- app/movement/movement.py
+++ app/movement/movement.py
@@ -12,15 +12,9 @@ class Movement:
         print(data)
         self.bus.write_i2c_block_data(self.address, 0, data)
         return -1
-<<<<<<< HEAD
         
     def readNumber(self):
         number = self.bus.read_byte(self.address)
-=======
-
-    def readNumber():
-        number = self.bus.read_byte(address)
->>>>>>> 747e1c33e84229024897a84830139cd8108bb0bc
         return number
 
     def move_servo(self, servo, position):
