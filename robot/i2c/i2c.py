70aa93fe689cd9cba6d43ba5a15980abfdd6491d
Dennis <dennis.vanderweide@gmail.com>
2019-05-13 16:18:55 +0200

    Data transfer working Co-authored-by: Erwin <schaaperwin@kpnplanet.nl>

--- robot/i2c/i2c.py
+++ robot/i2c/i2c.py
@@ -1,20 +1,29 @@
-from smbus2 import SMBus # Deze line zal crashen op Windows systemen.
+# from smbus2 import SMBus # Deze line zal crashen op Windows systemen.
+import smbus2
+import serial
+import time
 
 class I2C:
     def __init__(self, address):
-        self.bus = SMBus(1)
+        self.bus = smbus2.SMBus(1)
         self.address = address
         print("I2C Ready")
         
     def writeByteBlock(self, value):
         data = list(bytearray(value, 'ascii'))
-        print(data)
         self.bus.write_i2c_block_data(self.address, 0, data)
         
-    def readNumber(self):
-        number = self.bus.read_byte(self.address)
-        return number
+    def getStatus(self):
+        while True:
+            Temperature = self.read()
+            print("Temperature: ", Temperature)
+            Voltage = self.read()
+            print("Voltage: ", Voltage)
+            Position = self.read()
+            print("Position: ", Position*1023/255)
+            time.sleep(3)
+    
     
     def read(self):
-        data = self.bus.read_i2c_block_data(self.address, 0)
+        data = self.bus.read_byte(self.address)
         return data
