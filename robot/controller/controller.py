d4556a5d1ae02374449c936d567f9c247aa5112e
PietervdWerk <pieter.vdwerk@gmail.com>
2019-06-04 13:49:38 +0200

    SocketAI

--- robot/controller/controller.py
+++ robot/controller/controller.py
@@ -1,6 +1,7 @@
 from communication.socket import Socket
-from communication.i2c import I2C
-from movement.movement import Movement, map_position
+from communication.SocketAI import SocketAI
+#from communication.i2c import I2C
+#from movement.movement import Movement, map_position
 from threading import Thread
 import time
 import json
@@ -11,15 +12,16 @@ class Controller:
         print("Controller started")
         self.messages = []
         # Movement center with connection to Arduino
-        self.i2c = I2C(self, 4)
-        self.movement = Movement(self.i2c)
+        #self.i2c = I2C(self, 4)
+        #self.movement = Movement(self.i2c)
         
     
     def start(self):
         # Connection to webserver
-        Thread(target=Socket(self).start, daemon=True).start()
+        #Thread(target=Socket(self).start, daemon=True).start()
+        Thread(target=SocketAI(self).start, daemon=True).start()
         # Status checker
-        Thread(target=self.i2c.get_status, daemon=True).start()
+        #Thread(target=self.i2c.get_status, daemon=True).start()
         #self.movement.move_servo("97,100")
         input()
         
@@ -28,10 +30,10 @@ class Controller:
         type = j["type"]
         move = j["message"]["move"]
         # Choose appropriate movement command
-        if(type == "move_motor"):
-            self.movement.move(move)
-        elif(type == "move_arm"):
-            self.movement.move(move, "arm")
+        #if(type == "move_motor"):
+            #self.movement.move(move)
+        #elif(type == "move_arm"):
+            #self.movement.move(move, "arm")
             
     
     def send(self, json):
