b5230ec5add8dc5e3bc7624aa5eab7d3c80cc18b
Erwin Schaap <schaaperwin@kpnplanet.nl>
2019-06-07 08:59:59 +0200

    Socket AI integrated with testclient

--- robot/communication/SocketAI.py
+++ robot/communication/SocketAI.py
@@ -1,8 +1,13 @@
-import cv2
 import base64
 import websocket
 from threading import Thread
 import time
+
+from PIL import Image
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import io
+import numpy as np
 
 
 class SocketAI:
@@ -16,38 +21,47 @@ class SocketAI:
 
     def on_error(self):
         print("Error")
-        main()
+        self.start()
 
     def on_close(self):
         print("Close")
-        main()
+        self.start()
 
     def image_sender(self):
         running = True
 
         while running:
             try:
-                cam = cv2.VideoCapture(0)
+                camera = PiCamera()
+                camera.resolution = (384, 216)
+                rawCapture = PiRGBArray(camera, size=(384, 216))
 
-                while True:
-                    if 1:
-                        self.dinko = time.time()
-                        ret, frame = cam.read()
-                        frame = cv2.resize(frame, (384, 216))
-                        result, image = cv2.imencode('.jpg', frame)
-                        base64image = base64.b64encode(image)
-                        self.ws.send(base64image)
-                        self.ws.send("<EOF>")
-                        print("verzonden image")
-                        self.send = False
+                for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
+                    rawCapture.truncate()
+                    rawCapture.seek(0)
+                    self.dinko = time.time()
+                    img = Image.fromarray(frame.array)
+                    imgByteArr = io.BytesIO()
+                    img.save(imgByteArr, format='JPEG')
+
+                    base64image = base64.b64encode(imgByteArr.getvalue())
+                    self.ws.send("img ")
+                    self.ws.send(base64image)
+                    self.ws.send("<EOF>")
+                    print("Send")
+                    self.send = False
+                    rawCapture.truncate(0)
             except:
                 print("error while creating and sending images")
             finally:
-                cam.release()
                 running = False
 
     def on_open(self):
         print("Connected to server")
+        time.sleep(3)
+        self.ws.send("mode block")
+        self.ws.send("<EOF>")
+        time.sleep(3)
         Thread(target=self.image_sender).start()
 
     def handle_message(self, message):
@@ -58,11 +72,12 @@ class SocketAI:
             self.ws.run_forever()
             time.sleep(1)
 
-    def __init__(self, controller):
+    def __init__(self):
         print("Trying to connect to the server")
-        self.controller = controller
         self.ws = websocket.WebSocketApp("ws://141.252.29.41:5000/"
                                          , on_message=self.on_message
                                          , on_error=self.on_error
                                          , on_close=self.on_close)
         self.ws.on_open = self.on_open
+
+
